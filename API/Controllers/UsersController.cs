f602020e7752b85ba9c18d4d98d8758b6af1b6ea
unknown <rus3700@gmail.com>
2021-03-28 15:26:43 -0500

    ADD some changes.section4 enf

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,12 +5,11 @@ using  API.Entities;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -18,7 +17,9 @@ namespace API.Controllers
             _context = context;
         }
 
+        //Show all users
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
@@ -27,6 +28,7 @@ namespace API.Controllers
 
         // api/users/3
          [HttpGet("{id}")]
+         [Authorize]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
